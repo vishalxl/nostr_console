4b5c88fba49e58874d724dd5fd3143bdc8d38857
Vishal <64505169+vishalxl@users.noreply.github.com>
2022-11-20 13:43:06 +0530

    new 0.1.5 release

--- lib/settings.dart
+++ lib/settings.dart
@@ -3,7 +3,7 @@ import 'package:logging/logging.dart';
 
 // name of executable
 const String exename = "nostr_console";
-const String version = "0.1.4-beta";
+const String version = "0.1.5-beta";
 
 int gDebug = 0;
 int gSpecificDebug = 0;

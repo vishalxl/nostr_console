7546652a116f0246de87c8e26671e5564951fcc9
Vishal <64505169+vishalxl@users.noreply.github.com>
2024-03-17 18:06:44 +0530

    renamed relay list, and improved scripts

--- scripts/send_request.sh
+++ scripts/send_request.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-source ./configfile.cfg
+configfile="./relay_list_all.txt"
 
+if [[ $# -eq 0 ]] ; then
+    echo 'Usage: ./send_request.sh <nostr request> <nostr relays filename(optional)>'
+	echo 'nostr relays filename contains list of relays to use; its default value is relay_list_all.txt' 
+    exit 1
+fi
+
+if [[ $# -eq 2 ]] ; then
+	configfile=$2
+fi
+
+
+echo Going to use $configfile for list of relays to use.
+source ./$configfile
 
 # ./send_request.sh   '["REQ","name",{"ids":["b10180"]}]'
 #  ./send_request.sh '["REQ","nnn",{"limit":2,"ids":["d8aa6787834de19f0cb61b2aeef94886b2284f36f768bf8b5cc7533988346997"]}]'
 
+echo for loop
 for relay in ${nostr_relays[@]};
 do
 >&2 echo -e  "\n\n------------Sending $1 to $relay---------------------------\n"
--- scripts/test_servers.sh
+++ scripts/test_servers.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-source ./configfile.cfg
+configfile="./relay_list_all.txt"
+
+if [[ $# -eq 1 ]] ; then
+	if [ $1=="--help"  ]; then 
+		echo 'Usage: ./test_servers.sh  <nostr relays filename(optional)>'
+		echo 'nostr relays filename contains list of relays to test; its default value is relay_list_all.txt' 
+		exit 1
+	fi
+	
+	configfile=$1	
+fi
+
+source $configfile
 
 limit=300
 numHours=1
@@ -8,9 +20,6 @@ numHours=1
 echo -e "Requesting all events in last $numHours hours with a limit of $limit by executing the following command for each:"
 sinceSeconds=`date -d "-$numHours hour" +%s` ; 
 
-#3235
-#req="[\"REQ\",\"id_mention_#p_nostr.coinos.io\",{\"#p\":[\"3235036bd0957dfb27ccda02d452d7c763be40c91a1ac082ba6983b25238388c\"],\"limit\":20000,\"since\":1654569836},{\"authors\":[\"3235036bd0957dfb27ccda02d452d7c763be40c91a1ac082ba6983b25238388c\"]}]"
-#jack
 N=2
 inLastNDays=`date -d "$N days" +%s`
 
